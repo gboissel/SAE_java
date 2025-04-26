e6d6899bdbe06e544f2f67d83aa6f8ad91f9a8fa
gboissel <gauthier.boissel@etu.univ-orleans.fr>
2025-04-26 14:28:44 +0200

    maintenant il vaut utiliser junit :'(

--- src/Administrateur.java
+++ src/Administrateur.java
@@ -1,7 +1,9 @@
 public class Administrateur extends Utilisateur{
+    
     public Administrateur(String nom,String prenom,String mdp){
         super(nom,prenom,mdp);
     }
+
     @Override
     public String getRoles() {
         return "Administrateur";
