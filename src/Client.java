39af334d78e865ba88b8bb117d7c7eb464811dcb
EtienneA20 <etienne.audor@etu.univ-orleans.fr>
2025-05-15 11:50:08 +0200

    ok

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,9 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Client extends Utilisateur{
-    
+    private List<Commande> commandes;
     /**
      * Creer une instance d'un Utilisateur qui est un Client
      * @param nom
@@ -8,7 +12,7 @@ public class Client extends Utilisateur{
      */
     public Client(String nom,String prenom,String mdp){
         super(nom,prenom,mdp);
-
+        this.commandes = new ArrayList<>();
     }
     @Override
     public String getRoles() {
@@ -16,14 +20,17 @@ public class Client extends Utilisateur{
     }
 
     public void commander(Livre livre, String type, String liaison){
-
+    LocalDate dateActuelle = LocalDate.now();
+    this.commandes.add(new Commande(0, liaison, 0, 0, this, null))
     }
 
     public void modifierCommande(Commande commande, String liaison){
 
     }
 
-    //public List<Commander> consulterCommandes(){    }
+    public List<Commande> consulterCommandes(){  
+        return this.commandes;
+    }
 
     public void consulter(){
 
