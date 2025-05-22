2eea444bc97d9ddd63e7ef777d5a51438c34cbbf
AHameau01 <alexis.hameau@etu.univ-orleans.fr>
2025-05-22 11:35:04 +0200

    test

--- src/TestLibrairie.java
+++ src/TestLibrairie.java
@@ -1,10 +1,17 @@
 import org.junit.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 public class TestLibrairie {
     @Test
     public void TestAutentification(){
         Librairie lb = new Librairie();
-        assertEquals(lb.authentification("Doo", "Samie", "scooby", "Client"),lb.authentification("jarl", "Sam", "scooby", "Vendeur"));
+        lb.createClient("Doo", "Samie", "scooby");
+        lb.createVendeur("Jarl", "Sam", "vendeur123");
+
+        // Cas où l'authentification réussit
+        assertTrue(lb.authentification("D1oo", "Samie", "scooby", "Client"));
+        assertTrue(lb.authentification("Jarl", "Sam", "vendeur123", "Vendeur"));
     }
+    
 }
