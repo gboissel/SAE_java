ca95163dac8d02a5f23ebe5f12412c463b03ebdb
Ahameau01 <alexis.hameau@etu.univ-orleans.fr>
2025-06-09 20:10:11 +0200

    pareil

--- src/ExecutableTestArrache.java
+++ src/ExecutableTestArrache.java
@@ -1,13 +1,13 @@
 public class ExecutableTestArrache {
     public static void main(String[] args){
-/*         lib.createClient("pascal", "no", "1234");
+        Librairie lib = new Librairie();
+        lib.createClient("pascal", "no", "1234");
         lib.createClient("Jarl", "twt", "5");
         lib.createClient("Doo", "Samie", "scooby");
         lib.createClient("Doo", "Scooby", "croquette");
         lib.createVendeur("JAIME", "argent", "6789");
         System.out.println(lib.getUsers());
         System.out.println(lib.authentification("Doo", "Samie", "scooby", "Client"));
-        lib.authentificationConsole(); */
-        GestionConsole inst = new GestionConsole();
+        lib.authentificationConsole();
     }
 }
