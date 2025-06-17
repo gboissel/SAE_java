2150096b89bf35dd87d67b35e4ba88fd7c4bac64
AHameau01 <alexis.hameau@etu.univ-orleans.fr>
2025-06-17 16:37:52 +0200

    vueclient

---
+++ controleur/ControleurClientMagasin.java
@@ -0,0 +1,114 @@
+package controleur;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+// import javafx.beans.binding.Bindings;                                       \
+                                                                                
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import java.awt.event.ActionEvent; 
+import javafx.scene.input.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import modele.Librairie;
+import javafx.scene.image.ImageView;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+
+public class ControleurClientMagasin {
+    private ControleurClientMagasin ccm;
+    private Librairie lib;
+
+    @FXML
+    private Button btnDeco;
+
+    @FXML
+    private Button btnMag1;
+
+    @FXML
+    private Button btnMag2;
+
+    @FXML
+    private Button btnMag3;
+
+    @FXML
+    private Button btnMag4;
+
+    @FXML
+    private Button btnMag5;
+
+    @FXML
+    private Button btnMag6;
+    
+    @FXML
+    private Button btnMag7;
+
+    @FXML
+    private Button btnMag8;
+
+    @FXML
+    private Button btnMag9;
+
+    @FXML
+    public void controleurBoutDeco(ActionEvent e){
+        System.out.println("vous etes bien deconnecter");
+    }
+
+    @FXML
+    public void controleurBoutMag1(ActionEvent e){
+        System.out.println("vous etes sur le magasin1");
+    }
+
+    @FXML
+    public void controleurBoutMag2(ActionEvent e){
+        System.out.println("vous etes sur le magasin1");
+    } 
+
+    @FXML
+    public void controleurBoutMag3(ActionEvent e){
+        System.out.println("vous etes sur le magasin1");
+    } 
+
+    @FXML
+    public void controleurBoutMag4(ActionEvent e){
+        System.out.println("vous etes sur le magasin1");
+    }
+
+    @FXML
+    public void controleurBoutMag5(ActionEvent e){
+        System.out.println("vous etes sur le magasin1");
+    }
+
+    @FXML
+    public void controleurBoutMag6(ActionEvent e){
+        System.out.println("vous etes sur le magasin1");
+    }
+
+    @FXML
+    public void controleurBoutMag7(ActionEvent e){
+        System.out.println("vous etes sur le magasin1");
+    }
+
+    @FXML
+    public void controleurBoutMag8(ActionEvent e){
+        System.out.println("vous etes sur le magasin1");
+    }
+
+    @FXML
+    public void controleurBoutMag9(ActionEvent e){
+        System.out.println("vous etes sur le magasin1");
+    }
+    
+
+
+}
