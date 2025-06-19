9cf4c3ce544b01849be378b57e09ea9d1823ed0b
Gauthier <gauthier.boissel@etu.univ-orleans.fr>
2025-06-19 11:39:24 +0200

    changement page debug

--- src/controleur/ControleurClient1.java
+++ src/controleur/ControleurClient1.java
@@ -1,5 +1,6 @@
 package controleur;
-import exception.*;
+import java.util.Arrays;
+import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.fxml.FXML;
@@ -49,55 +50,62 @@ public class ControleurClient1 extends Controleur{
 
     @FXML
     public void controleurBoutMag1(ActionEvent e){
-        this.modele.setCurMag(this.modele.getMagasins().get(0+this.nbPage));
-        System.out.println(this.modele.getMagasins().get(0+this.nbPage).getNom());
+        this.modele.setCurMag(this.modele.getMagasins().get(0+this.nbPage*9));     
         this.vue.changerVue("/view/VuePageClient2.fxml");
     }
 
     @FXML
     public void controleurBoutMag2(ActionEvent e){
-        System.out.println("vous etes sur le magasin2");
+        this.modele.setCurMag(this.modele.getMagasins().get(1+this.nbPage*9));
+        this.vue.changerVue("/view/VuePageClient2.fxml");
     } 
 
     @FXML
     public void controleurBoutMag3(ActionEvent e){
-        System.out.println("vous etes sur le magasin3");
+        this.modele.setCurMag(this.modele.getMagasins().get(2+this.nbPage*9));
+        this.vue.changerVue("/view/VuePageClient2.fxml");
     } 
 
     @FXML
     public void controleurBoutMag4(ActionEvent e){
-        System.out.println("vous etes sur le magasin4");
+        this.modele.setCurMag(this.modele.getMagasins().get(3+this.nbPage*9));
+        this.vue.changerVue("/view/VuePageClient2.fxml");
     }
 
     @FXML
     public void controleurBoutMag5(ActionEvent e){
-        System.out.println("vous etes sur le magasin5");
+        this.modele.setCurMag(this.modele.getMagasins().get(4+this.nbPage*9));
+        this.vue.changerVue("/view/VuePageClient2.fxml");
     }
 
     @FXML
     public void controleurBoutMag6(ActionEvent e){
-        System.out.println("vous etes sur le magasin6");
+        this.modele.setCurMag(this.modele.getMagasins().get(5+this.nbPage*9));
+        this.vue.changerVue("/view/VuePageClient2.fxml");
     }
 
     @FXML
     public void controleurBoutMag7(ActionEvent e){
-        System.out.println("vous etes sur le magasin7");
+        this.modele.setCurMag(this.modele.getMagasins().get(6+this.nbPage*9));
+        this.vue.changerVue("/view/VuePageClient2.fxml");
     }
 
     @FXML
     public void controleurBoutMag8(ActionEvent e){
-        System.out.println("vous etes sur le magasin8");
+        this.modele.setCurMag(this.modele.getMagasins().get(7+this.nbPage*9));
+        this.vue.changerVue("/view/VuePageClient2.fxml");
     }
 
     @FXML
     public void controleurBoutMag9(ActionEvent e){
-        System.out.println("vous etes sur le magasin9");
+        this.modele.setCurMag(this.modele.getMagasins().get(8+this.nbPage*9));
+        this.vue.changerVue("/view/VuePageClient2.fxml");
     }
     @FXML
     public void pagePrec(){
         if (this.nbPage>0) {
             this.nbPage-=1;
-            this.chargerPage();
+            this.maJ();
         }
 
     }
@@ -105,91 +113,32 @@ public class ControleurClient1 extends Controleur{
     @FXML
     public void pageSuivante(){
         this.nbPage+=1;
-        this.chargerPage();
+        this.maJ();
     }
     @FXML
     private void accesPanier(){
     }
+
+    private void maJ(){
+        int dep = nbPage*9;
+        int compteur = 0;
+        String nomMag;
+        List<Button> btnL = Arrays.asList(this.btnMag1,this.btnMag2,this.btnMag3,this.btnMag4,this.btnMag5,this.btnMag6,this.btnMag7,this.btnMag8,this.btnMag9);
+        for(Button btn:btnL){
+            if (compteur<10 && (nbPage*9+compteur)<this.modele.getMagasins().size()){
+                nomMag = this.modele.getMagasins().get(dep+compteur).getNom();
+                btn.setText(nomMag);
+                btn.setDisable(false);
+            }else{
+                btn.setDisable(true);
+                btn.setText("N/A");
+            }++compteur;
+        }
+    }              
+
     @Override
     public void chargerPage(){
         this.nbPage = 0;
-        int dep = nbPage*9;
-        String nomMag;        
-        for (int i=nbPage*9;i<(nbPage+1)*9;i++){// sa s'execute 9 fois pour chaque page
-        if ((this.modele.getMagasins().size()>nbPage*9+i)){
-            switch ((i+dep)%9){
-                case 0:
-                    if (this.modele.getMagasins().size()>nbPage*9+i){
-                        nomMag = this.modele.getMagasins().get(i+dep%9).getNom();
-                        this.btnMag1.setText(nomMag);
-                    }else{
-                        this.btnMag1.setText("A venir ... Il n'y a plus de magasin.");
-                    } 
-                case 1:
-                    if (this.modele.getMagasins().size()>nbPage*9+i){
-                    nomMag = this.modele.getMagasins().get(i+dep%9).getNom();
-                        this.btnMag2.setText(nomMag);
-                    }else
-                        this.btnMag2.setText("A venir ... Il n'y a plus de magasin.");
-                    
-                case 2:
-                    if (this.modele.getMagasins().size()>nbPage*9+i){
-                        nomMag = this.modele.getMagasins().get(i+dep%9).getNom();
-                        this.btnMag3.setText(nomMag);
-                    }else{
-                        this.btnMag3.setText("A venir ... Il n'y a plus de magasin.");
-                    }
-                    
-                case 3:
-                    if (this.modele.getMagasins().size()>nbPage*9+i){
-                        nomMag = this.modele.getMagasins().get(i+dep%9).getNom();
-                        this.btnMag4.setText(nomMag);
-                    }else{
-                        this.btnMag4.setText("A venir ... Il n'y a plus de magasin.");
-                    }
-                    
-                case 4:
-                    if (this.modele.getMagasins().size()>nbPage*9+i){
-                        nomMag = this.modele.getMagasins().get(i+dep%9).getNom();
-                        this.btnMag5.setText(nomMag);
-                    }else{
-                        this.btnMag5.setText("A venir ... Il n'y a plus de magasin.");
-                    }
-                    
-                case 5:
-                    if (this.modele.getMagasins().size()>nbPage*9+i){
-                        nomMag = this.modele.getMagasins().get(i+dep%9).getNom();
-                        this.btnMag6.setText(nomMag);}
-                    else{
-                        this.btnMag6.setText("A venir ... Il n'y a plus de magasin.");
-                    }
-                    
-                case 6:
-                    if (this.modele.getMagasins().size()>nbPage*9+i){
-                        nomMag = this.modele.getMagasins().get(i+dep%9).getNom();
-                        this.btnMag7.setText(nomMag);}
-                    else{
-                        this.btnMag7.setText("A venir ... Il n'y a plus de magasin.");
-                    }
-                        
-                case 7:
-                    if (this.modele.getMagasins().size()>nbPage*9+i){
-                        nomMag = this.modele.getMagasins().get(i+dep%9).getNom();
-                        this.btnMag8.setText(nomMag);
-                    }else{
-                        this.btnMag8.setText("A venir ... Il n'y a plus de magasin.");
-                    }
-                    
-                case 8:
-                    if (this.modele.getMagasins().size()>nbPage*9+i){
-                    nomMag = this.modele.getMagasins().get(i+dep%9).getNom();
-                        this.btnMag9.setText(nomMag);
-                    }else{
-                        this.btnMag9.setText("A venir ... Il n'y a plus de magasin.");
-                    }
-                
-            }}
-            
-        }
+        this.maJ();
     }
 }
